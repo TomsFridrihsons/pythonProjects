80913de957691c632e09852048d2bf34e7cb4174
Toms <fridrihsonst@gmail.com>
2022-07-09 11:14:13 +0300

    Hang game remade and ready to play

--- HangMan/main.py
+++ HangMan/main.py
@@ -1,53 +1,18 @@
-#TODO  1)Find a database with words
+#TODO  1)Find a database with words: DONE
 #TODO  2)Create functions for
-#           input
-#           randomly choosing the guessing word from dictionary
-#           checking each letter (if in correct place or if it is in the word)
-#           Displaying the game
+#           input: DONE
+#           randomly choosing the guessing word from dictionary: DONE
+#           checking each letter (if in correct place or if it is in the word and how many times): DONE
+#           Displaying the game: DONE
 #           Statistics
 #           Check if input word exists in database. If it does not then the word does not exists and
 #               let the player try again
 #           Create "Next word" function
+#           Make it object oriented
 import random
 import csv
 
-def wordInput(wordLenght, allowedGuesses):
-    guessedWord = ""
-    print(f"Tev ir jāuzmin vārds ar {wordLenght} burtiem")
-    inputWord = input(f"Tev ir palikuši {allowedGuesses} mēģinājumi: ")
-    for letter in inputWord:
-        if (letter.isalpha()) == True:
-            guessedWord += letter
-
-    guessedWord = guessedWord.lower()
-
-    if wordLenght != len(guessedWord):
-        print("Ievadītais vārds neatbilst mināmā vārda garumam.")
-        print("Mēģini vēlreiz!")
-        wordInput(wordLenght, allowedGuesses)
-    else:
-        return guessedWord
-
-def getWordMain():
-    rowCount = 0
-    gameInfo = []
-    DictionaryPath = "./latvianNounDict.csv"
-    openReader = open(DictionaryPath, 'r', encoding="utf8")
-    reader = csv.reader(openReader)
-    allWords = list(reader)
-    for row in allWords:
-        rowCount+=1
-    rowNumber = random.randint(0, rowCount-1)
-    chosenWord = allWords[rowNumber][0]
-    if len(chosenWord) >= 6:
-        print("Pa garu")
-        getWord()
-    openReader.close()
-
-    return chosenWord
-
-
-def game():
+def menu():
     print()
     print("KARĀTAVAS")
     print("Izvēlne:")
@@ -55,62 +20,112 @@ def game():
     print("2 --> Apskatīt statistiku")
     print("3 --> Apskatīt noteikumus")
     print("4 --> Beigt spēli")
-    choise = input()
-    choise = int(choise)
-    if choise >= 1 and choise <= 3:
-        if choise == 1:
-            while choise != 3:
+    choice = input()
+    try:
+        choice = int(choice)
+    except ValueError:
+        choice = "n"
+
+    if type(choice) == int:
+        choice = int(choice)
+        if choice >= 1 and choice <= 3:
+            if choice == 1:
                 print("Sākam spēli!")
-                full()
                 game()
-    else:
-        print("Try again!")
-        game()
+            if choice == 3:
+                print("Beigt sēli!")
+                return 0
+                # game()
+        else:
+            print("Try again!")
+            menu()
 
 
-def inputCheck(chosenWord, inputWord, guessCount):
-    if guessCount == 6:
-        correctLetters = 0
-        correctGuess = []
-    if chosenWord == inputWord:
-        return "Guessed!"
-    else:
-        for letter in range(len(inputWord)):
-            correctGuess.append("_")
-            if chosenWord[letter] == inputWord[letter]:
-                correctLetters += 1
-                correctGuess[letter] = chosenWord[letter]
-        print(f"Tu pareizi uzminēji {correctLetters} burtus")
-        return correctGuess
-
-
-def full():
-    print()
-    print()
+def gameSetup():
+    correctLetters = 0
+    correctGuess = []
     guessCount = 6
     gameCount = 0
-    result = ""
     chosenWord = getWord()
     wordLenght = len(chosenWord)
-    print(f"Vārda garums {wordLenght}")
-    print(f"Mēģinājumu skaits {guessCount}")
+
+    for letter in range(len(chosenWord)):
+        correctGuess.append("_")
+
+    gameSetup = [correctLetters, correctGuess, chosenWord, guessCount, gameCount, wordLenght]
+    return gameSetup
+
+
+def letterInput():
+    userInput = input(f"Ievadi burtu: ")
+    inputLetter = userInput[0]
+    if (inputLetter.isalpha()) == True:
+        inputLetter = inputLetter.lower()
+        return inputLetter
+    else:
+        print("Ievadītais simbols nav burts.")
+        print("Mēģini vēlreiz!")
+        return False
+
+
+def gameCheck(gameSetup, inputLetter):
+    currentTry = 0
+    unguessedLetters = 0
+    timesInWord = 0
+    knownLetter = False
+    if inputLetter in gameSetup[1]:
+        knownLetter = True
+    for letter in range(len(gameSetup[1])):
+        if knownLetter == True:
+            print("Šis burts jau ir atminēts!")
+            break
+        if gameSetup[1][letter] == "_":
+            if gameSetup[2][letter] == inputLetter:
+                currentTry = 1
+                gameSetup[0] += 1
+                gameSetup[1][letter] = gameSetup[2][letter]
+                timesInWord += 1
+
+    if currentTry == 0 and knownLetter == False:
+        gameSetup[3] -= 1
+        print(f"Šis burts nav vārdā, kas ir jāatmin.")
+    else:
+        print(f"Šis burts vārdā atrodas {timesInWord} reizes")
+
+    print(f"Tu pareizi esi uzminējis {gameSetup[0]} no {gameSetup[5]} burtiem.")
+    print(f"Uzminētie burti: {gameSetup[1]}.")
+    print(f"Atlikušo mēģinājumu skaits: {gameSetup[3]}.")
+
+    if '_' in gameSetup[1]:
+        return False
+    else:
+        return True
+
+
+def game():
+
+    result = True
+    gameInfo = gameSetup()
     print()
-    for game in range(guessCount):
-        inputWord = wordInput(wordLenght, guessCount-game)
-        print(inputWord)
-        if inputWord is None:
-            print("Notika kļūda. Mēģini vēlreiz!")
-            inputWord = wordInput(wordLenght, guessCount - game)
-        result = inputCheck(chosenWord, inputWord, guessCount)
-        if type(result) is not list:
+    print()
+    print(f"Vārda garums: {gameInfo[5]}")
+    print(f"Mēģinājumu skaits: {gameInfo[3]}")
+    print()
+    for tryNr in range(gameInfo[3]):
+        inputLetter = False
+        while type(inputLetter) == bool or inputLetter is None:
+            if inputLetter is None:
+                print("Notika kļūda. Mēģini vēlreiz!")
+            inputLetter = letterInput()
+        print(inputLetter)
+        result = gameCheck(gameInfo, inputLetter)
+        if result == True:
             print("Congrats you won!")
             break
-        else:
-            print(result)
 
-    if type(result) is list:
+    if result == False:
         print("Tu neuzminēji doto vārdu!")
-        print(f"Vārds, kas Tev bija jāuzmin: {chosenWord}")
+        print(f"Vārds, kas Tev bija jāuzmin: {gameInfo[2]}")
 
 
 def getWord():
@@ -128,11 +143,10 @@ def getWord():
     # allWords = list(reader)
     wordNumber = random.randint(0, rowCount-1)
     chosenWord = allWords[wordNumber]
-    print(chosenWord)
     openReader.close()
     return chosenWord
 
 
-game()
+menu()
 
 
